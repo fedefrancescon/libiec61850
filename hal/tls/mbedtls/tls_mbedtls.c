0ccd1b5dd9176cbb26d32e0476bbb4b194d169ea
Federico Francescon <fede.francescon@gmail.com>
2026-07-29 09:08:13 +0200

    revert: restore merge removal of debug print function
    Restore mbedtls debug print function, wrongly reoved during merge

--- hal/tls/mbedtls/tls_mbedtls.c
+++ hal/tls/mbedtls/tls_mbedtls.c
@@ -68,6 +68,21 @@ tlsFormatTimestamp(char* buffer, size_t bufferSize)
         millis);
 }
 
+static void debug_print_mbedtls(void *ctx, int level, const char *file, int line, const char *str)
+{
+    const char *p, *basename;
+    (void) ctx;
+
+    /* Extract basename from file */
+    for(p = basename = file; *p != '\0'; p++) {
+        if(*p == '/' || *p == '\\') {
+            basename = p + 1;
+        }
+    }
+
+    mbedtls_printf("%s:%04d: |%d| %s", basename, line, level, str);
+}
+
 #define DEBUG_PRINT(appId, fmt, ...) do { \
         char _timestamp[32]; \
         tlsFormatTimestamp(_timestamp, sizeof(_timestamp)); \
